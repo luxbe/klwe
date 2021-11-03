261383c853058b01a2cc8ce85c1a8a3adf5f26ba
luxbe <luxbe@tutanota.com>
2021-11-03 22:16:21 +0100

    improve EditorPanel with keyboard shortcuts
    - make ItemEntry list navigatable by tab
    - add F2 to rename functionality

--- src/components/Panel/EditorPanel/EditorPanel.svelte
+++ src/components/Panel/EditorPanel/EditorPanel.svelte
@@ -7,35 +7,56 @@
     import ItemEntry from './ItemEntry';
 
     const { flatItems, selectedItem } = kustomStore;
-    let hasFocus = false;
+    let hasFocus = false,
+        currentIndex = -1;
 
-    function onAddButtonClick() {
-        dialogStore.show(NewItemDialog);
+    const itemElList: { focus(): Promise<void> }[] = [];
+
+    function moveCurIndex(backward = false) {
+        currentIndex += backward ? -1 : 1;
+        if (currentIndex >= $flatItems.length) currentIndex = 0;
+        if (currentIndex < 0) currentIndex = $flatItems.length - 1;
+
+        selectedItem.set($flatItems[currentIndex]);
     }
 
-    function onTreeClick() {
-        hasFocus = true;
-    }
+    function onKeydown(e: KeyboardEvent) {
+        if (!hasFocus) return;
 
-    function onTreeOutsideClick() {
-        hasFocus = false;
-    }
-
-    function onKeyup(e: KeyboardEvent) {
-        if (hasFocus && $selectedItem && e.key === 'Delete') {
-            kustomService.removeItem($selectedItem);
+        if (e.key === 'Tab') {
+            e.preventDefault();
+            moveCurIndex(e.shiftKey);
+        } else if ($selectedItem) {
+            if (e.key === 'Delete') kustomService.removeItem($selectedItem);
+            else if (e.key === 'F2')
+                currentIndex >= 0 && itemElList[currentIndex].focus();
         }
     }
 </script>
 
-<svelte:window on:keyup={onKeyup} />
+<svelte:window on:keydown={onKeydown} />
 
-<div class="tree" on:click={onTreeClick} use:outsideClick={onTreeOutsideClick}>
-    {#each $flatItems as item}
-        <ItemEntry {item} />
+<div
+    class="tree"
+    on:click|stopPropagation={() => (hasFocus = true)}
+    use:outsideClick={() => {
+        hasFocus = false;
+        currentIndex = -1;
+    }}
+>
+    {#each $flatItems as item, i}
+        <ItemEntry
+            {item}
+            active={currentIndex === i}
+            on:click={() => {
+                currentIndex = i;
+                selectedItem.set($flatItems[i]);
+            }}
+            bind:itemEl={itemElList[i]}
+        />
     {/each}
     <div class="spacer" on:click={() => ($selectedItem = undefined)} />
-    <button class="btn is-icon" on:click={onAddButtonClick}
+    <button class="btn is-icon" on:click={() => dialogStore.show(NewItemDialog)}
         ><Icon icon="add" /></button
     >
 </div>
--- src/components/Panel/EditorPanel/ItemEntry/ItemEntry.svelte
+++ src/components/Panel/EditorPanel/ItemEntry/ItemEntry.svelte
@@ -2,15 +2,20 @@
     import Icon from '@/components/Icon';
     import { InlineInput } from '@/components/Input';
     import { kustomService, dataService } from '@/services';
-    import { kustomStore } from '@/stores';
     import { Item, Overlap, Stack } from 'kustom';
     import { tick } from 'svelte';
 
     import { HasLevel, KLWE_ATTRIBUTE } from '@/interfaces';
 
-    const { selectedItem } = kustomStore;
+    export let item: Item & HasLevel, active: boolean;
 
-    export let item: Item & HasLevel;
+    export const itemEl = {
+        async focus() {
+            titleInputIsDisabled = false;
+            await tick();
+            inputEl.select();
+        },
+    };
 
     $: placeholder = dataService.getPlaceholderTitle(item);
     $: isFolder = !!(item as Overlap | Stack).viewgroup_items;
@@ -19,21 +24,15 @@
 
     let inputEl: HTMLInputElement,
         titleInputIsDisabled = true;
-
-    async function onNodeDoubleClick() {
-        titleInputIsDisabled = false;
-        await tick();
-        inputEl.select();
-    }
 </script>
 
 <div
     class="tree__node"
     class:open={isOpen}
-    class:active={$selectedItem === item}
+    class:active
     style="padding-left: {item[KLWE_ATTRIBUTE].level}rem;"
-    on:click={() => ($selectedItem = item)}
-    on:dblclick={onNodeDoubleClick}
+    on:click
+    on:dblclick={itemEl.focus}
 >
     <div class="node__info">
         {#if isFolder}
